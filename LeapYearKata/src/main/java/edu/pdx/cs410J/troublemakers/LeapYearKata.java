5d7d5fd6d0b8d160596ddb729398758ad1422e2e
Erik Haake <ehaake@pdx.edu>
2020-01-22 18:37:51 -0800

    Refactor to LeapYearKata

---
+++ LeapYearKata/src/main/java/edu/pdx/cs410J/troublemakers/LeapYearKata.java
@@ -0,0 +1,37 @@
+package edu.pdx.cs410J.troublemakers;
+
+import edu.pdx.cs410J.lang.Human;
+
+import java.util.ArrayList;
+                                                                                    
+/**                                                                                 
+ * This class is represents a <code>LeapYearKata</code>.                                 
+ */                                                                                 
+public class LeapYearKata {
+
+  public LeapYearKata() {
+  }
+
+  public String says() {
+    throw new UnsupportedOperationException("Not implemented yet");
+  }
+                                                                                    
+  /**                                                                               
+   * Returns a <code>String</code> that describes this                              
+   * <code>LeapYearKata</code>.                                                          
+   */                                                                               
+  public String toString() {
+    return " ";
+  }
+
+  /**
+   * Main program that parses the command line, creates a
+   * <code>LeapYearKata</code>, and prints a description of the LeapYearKata to
+   * standard out by invoking its <code>toString</code> method.
+   */
+  public static void main(String[] args) {
+
+
+    System.exit(0);
+  }
+}
