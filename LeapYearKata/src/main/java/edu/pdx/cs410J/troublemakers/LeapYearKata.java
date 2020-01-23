504320f99721ef77f205c41d7ae20b7e5be41964
Erik Haake <ehaake@pdx.edu>
2020-01-22 18:39:02 -0800

    Remove boilerplate from LeapYearKata

--- LeapYearKata/src/main/java/edu/pdx/cs410J/troublemakers/LeapYearKata.java
+++ LeapYearKata/src/main/java/edu/pdx/cs410J/troublemakers/LeapYearKata.java
@@ -9,28 +9,7 @@ import java.util.ArrayList;
  */                                                                                 
 public class LeapYearKata {
 
-  public LeapYearKata() {
-  }
-
-  public String says() {
-    throw new UnsupportedOperationException("Not implemented yet");
-  }
-                                                                                    
-  /**                                                                               
-   * Returns a <code>String</code> that describes this                              
-   * <code>LeapYearKata</code>.                                                          
-   */                                                                               
-  public String toString() {
-    return " ";
-  }
-
-  /**
-   * Main program that parses the command line, creates a
-   * <code>LeapYearKata</code>, and prints a description of the LeapYearKata to
-   * standard out by invoking its <code>toString</code> method.
-   */
   public static void main(String[] args) {
-
 
     System.exit(0);
   }
